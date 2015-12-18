5478324c8776fdeb7f4fbe9e573aa143bd5e0051
Bernardo <bplmp@users.noreply.github.com>
2015-12-17 21:47:17 -0500

    Download images from Imgur.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var fs = require('fs');
+var request = require('request');
 var GoogleSpreadsheet = require('google-spreadsheet');
+var async = require('async');
 
 var sheetId = fs.readFileSync('sheet_id.txt', 'utf8');
 
-console.log(sheetId);
-
-String.prototype.cleanupDash = function() {
-  return this.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "-");
-}
+console.log('Accessing google sheet id ' + sheetId);
 
 // spreadsheet key is the long id in the sheets URL
 var my_sheet = new GoogleSpreadsheet(sheetId);
@@ -22,12 +20,13 @@ my_sheet.getRows( 1, function(err, rows){
 
 function writeMd(element, index, array) {
   var row = element;
-  //begin front matter
+  // begin front matter
   var md = "---";
   md += "\n" + "layout: project-page";
   md += "\n" + "title: " + row.yourprojecttitle;
-  md += "\n" + "linkname: " + row.yourprojecttitle.cleanupDash(); //this will be the project permalink
-  md += "\n" + "author: " + row.yourfirstname + " " + row.yourlastname;
+  var projectSlug = cleanupDash(row.yourprojecttitle);
+  md += "\n" + "linkname: " + projectSlug; //this will be the project permalink
+  md += "\n" + "author: " + row.projectauthor;
   md += "\n" + "tagline: " + row.shortdescriptionofyourproject;
   md += "\n" + "location: " + row.projectlocations;
   md += "\n" + "project-link:";
@@ -35,63 +34,95 @@ function writeMd(element, index, array) {
   for (var i = 0; i < links.length; i++) {
     md += "\n" + "    - href: " + links[i];
   }
-  md += "\n" + "embed-link: " + row.wantyourprojectembeddedonthewebsite;
   md += "\n" + "tags: " + row.projecttags;
-  md += "\n" + "thumbnail-path: ../../img/" + row.yourfirstname.toLowerCase() + "\/1.jpg";
-  md += "\n" + "img-folder: ../../img/" + row.yourfirstname.toLowerCase() +"\/";
+  var mainImage = parseImgLink(row.mainprojectimage);
+  md += "\n" + "thumbnail-path: ../../img/" + projectSlug + '/' + extractFileName(mainImage);
+  md += "\n" + "img-folder: ../../img/" + projectSlug + '/';
   md += "\n" + "timestamp: " + row.timestamp;
   md += "\n" + "---";
-  //end of front matter
-  var body = row.fullprojectdescription
-  var images = body.match(/(\bimg)\d+/ig);
-  for (var i = 0; i < images.length; i++) {
-    var digits = images[i].match(/\d+/g)[0];
-    var path = '![]({{ page.img-folder }}' + digits + '.jpg)';
-    body = body.replace(images[i], path);
+  // end of front matter
+
+  // create folders for project images
+  var projectFolder = 'responses/img/' + projectSlug;
+  if (!fs.existsSync('responses/img/')){
+    fs.mkdirSync('responses/img/')
   }
-  // md += "\n" + row.fullprojectdescription;
+  if (!fs.existsSync(projectFolder)){
+    fs.mkdirSync(projectFolder)
+  }
+
+  // download main image
+  download(mainImage, projectFolder + '\/' + extractFileName(mainImage), function(){
+      console.log('downloaded main image ' + extractFileName(mainImage));
+  });
+
+  // start of body
+  var body = row.fullprojectdescription
+  var images = body.match(/[^\s]*imgur[^\s]*/ig);
+
+  // download images
+  if (images != undefined) {
+    async.each(images, function(image) {
+      var imageUrl = parseImgLink(image);
+      var fileName = extractFileName(imageUrl);
+      download(imageUrl, projectFolder + '\/' + fileName, function(){
+          console.log('downloaded ' + fileName);
+      });
+      var mdImg = '![]({{ page.img-folder }}' + fileName + ')';
+      body = body.replace(image, mdImg); // replace url in body for md link to downloaded file
+    }, function(err){
+        // if any of the file processing produced an error, err would equal that error
+        if( err ) {
+          // One of the iterations produced an error.
+          // All processing will now stop.
+          console.log('A file failed to process');
+        } else {
+          console.log('All files have been processed successfully');
+        }
+    });
+  }
+
   md += "\n" + body;
-  //end of body
-  fs.writeFile('responses/' + row.yourprojecttitle.cleanupDash() + '.md', md, function (err) {
+  // end of body
+
+  // write project md file
+  fs.writeFile('responses/' + projectSlug + '.md', md, function (err) {
     if (err) throw err;
-    console.log('Saved ' + row.yourprojecttitle.cleanupDash() + ' project.');
+    console.log('Saved ' + projectSlug + ' project.');
   });
 }
 
+function parseImgLink(link) {
+  if (link.search('.png') > -1 ||
+      link.search('.jpg') > -1 ||
+      link.search('.jpeg') > -1 ||
+      link.search('.gif') > -1) {
+    return link;
+  } else if (link.search('imgur.com') >  -1) {
+    return link.replace('imgur.com', 'i.imgur.com') + '.jpg';
+  } else {
+    return link;
+  }
+}
 
+// http://stackoverflow.com/questions/12740659/downloading-images-with-node-js
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    // console.log('content-type:', res.headers['content-type']);
+    // console.log('content-length:', res.headers['content-length']);
+    if (res.headers['content-type'].search('image') > -1) {
+      // var ext = '.' + res.headers['content-type'].split('/')[1];
+      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    }
+  });
+};
 
-// // With auth -- read + write
-// // see below for authentication instructions
-// var creds = require('./google-generated-creds.json');
-// // OR, if you cannot save the file locally (like on heroku)
-// var creds = {
-//   client_email: 'yourserviceaccountemailhere@google.com',
-//   private_key: 'your long private key stuff here'
-// }
-//
-// my_sheet.useServiceAccountAuth(creds, function(err){
-// 	// getInfo returns info about the sheet and an array or "worksheet" objects
-// 	my_sheet.getInfo( function( err, sheet_info ){
-// 		console.log( sheet_info.title + ' is loaded' );
-// 		// use worksheet object if you want to stop using the # in your calls
-//
-// 		var sheet1 = sheet_info.worksheets[0];
-// 		sheet1.getRows( function( err, rows ){
-// 			rows[0].colname = 'new val';
-// 			rows[0].save();	//async and takes a callback
-// 			rows[0].del();  //async and takes a callback
-// 		});
-// 	});
-//
-// 	// column names are set by google and are based
-//   // on the header row (first row) of your sheet
-// 	my_sheet.addRow( 2, { colname: 'col value'} );
-//
-// 	my_sheet.getRows( 2, {
-// 		start: 100,			 // start index
-// 		num: 100,			   // number of rows to pull
-// 		orderby: 'name'  // column to order results by
-// 	}, function(err, row_data){
-// 		// do something...
-// 	});
-// })
+function extractFileName(url) {
+  var lastSlash = url.lastIndexOf('\/');
+  var fileName = url.substring(lastSlash + 1, url.length);
+  return fileName;
+}
+
+function cleanupDash(string) {
+  return string.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "-");
+}
