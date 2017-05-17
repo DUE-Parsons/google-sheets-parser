97093a664981b128de6cb6aefdd52de7efa30704
Eric Brelsford <ebrelsford@gmail.com>
2017-05-17 11:41:51 -0400

    Use impurge to get album image URLs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var request = require('request');
 var GoogleSpreadsheet = require('google-spreadsheet');
 var async = require('async');
+var impurge = require('impurge');
 
 var sheetId = fs.readFileSync('sheet_id.txt', 'utf8').trim();
 
@@ -13,46 +14,18 @@ var my_sheet = new GoogleSpreadsheet(sheetId);
 // Without auth -- read only
 // IMPORTANT: See note below on how to make a sheet public-readable!
 // # is worksheet id - IDs start at 1
-my_sheet.getRows( 1, function(err, rows){
+my_sheet.getRows(1, function(err, rows) {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  console.log( 'pulled in ' + rows.length + ' rows');
+  console.log('pulled in ' + rows.length + ' rows');
   rows.forEach(writeMd);
 });
 
 function writeMd(element, index, array) {
   var row = element;
-  // begin front matter
-  var md = "---";
-  md += "\n" + "layout: project-page";
-  md += "\n" + "title: " + '"' + row.yourprojecttitle + '"';
   var projectSlug = cleanupDash(row.yourprojecttitle);
-  md += "\n" + "linkname: " + projectSlug; //this will be the project permalink
-  md += "\n" + "author: " + '"' + row.projectauthor + '"';
-  md += "\n" + "tagline: " + '"' + row.shortdescriptionofyourproject + '"';
-  md += "\n" + "location:";
-  var places = row.projectlocations.split(';');
-  for (var i = 0; i < places.length; i++) {
-    md += "\n" + "    - place: " + places[i];
-  }
-  md += "\n" + "project-link:";
-  var links = row.alinktoyourproject.split(';');
-  for (var i = 0; i < links.length; i++) {
-    md += "\n" + "    - href: " + links[i];
-  }
-  md += "\n" + "tags:";
-  var tags = row.projecttags.split(';');
-  for (var i = 0; i < tags.length; i++) {
-    md += "\n" + "    - tag: " + tags[i];
-  }
-  var mainImage = parseImgLink(row.mainprojectimage);
-  md += "\n" + "thumbnail-path: img/" + projectSlug + '/' + extractFileName(mainImage);
-  md += "\n" + "img-folder: ../../img/" + projectSlug + '/';
-  md += "\n" + "timestamp: " + row.timestamp;
-  md += "\n" + "---";
-  // end of front matter
 
   // create folders for project images
   var projectFolder = 'responses/img/' + projectSlug;
@@ -66,58 +39,77 @@ function writeMd(element, index, array) {
     fs.mkdirSync(projectFolder)
   }
 
-  // download main image
-  download(mainImage, projectFolder + '\/' + extractFileName(mainImage), function(){
-      console.log('downloaded main image ' + extractFileName(mainImage));
-  });
+  impurge.purge(row.mainprojectimage, function (error, urls) {
+    var mainImage = urls[0];
 
-  // start of body
-  var body = row.fullprojectdescription
-  var images = body.match(/[^\s]*imgur[^\s]*/ig);
+    // begin front matter
+    var md = "---";
+    md += "\n" + "layout: project-page";
+    md += "\n" + "title: " + '"' + row.yourprojecttitle + '"';
+    md += "\n" + "linkname: " + projectSlug; //this will be the project permalink
+    md += "\n" + "author: " + '"' + row.projectauthor + '"';
+    md += "\n" + "tagline: " + '"' + row.shortdescriptionofyourproject.replace(/"/g, '\\"') + '"';
+    md += "\n" + "location:";
+    var places = row.projectlocations.split(';');
+    for (var i = 0; i < places.length; i++) {
+      md += "\n" + "    - place: " + places[i];
+    }
+    md += "\n" + "project-link:";
+    var links = row.alinktoyourproject.split(';');
+    for (var i = 0; i < links.length; i++) {
+      md += "\n" + "    - href: " + links[i];
+    }
+    md += "\n" + "tags:";
+    var tags = row.projecttags.split(';');
+    for (var i = 0; i < tags.length; i++) {
+      md += "\n" + "    - tag: " + tags[i];
+    }
+    md += "\n" + "thumbnail-path: img/" + projectSlug + '/' + extractFileName(mainImage);
+    md += "\n" + "img-folder: ../../img/" + projectSlug + '/';
+    md += "\n" + "timestamp: " + row.timestamp;
+    md += "\n" + "---";
+    // end of front matter
 
-  // download images
-  if (images != undefined) {
-    async.each(images, function(image) {
-      var imageUrl = parseImgLink(image);
-      var fileName = extractFileName(imageUrl);
-      download(imageUrl, projectFolder + '\/' + fileName, function(){
-          console.log('downloaded ' + fileName);
-      });
-      var mdImg = '![]({{ page.img-folder }}' + fileName + ')';
-      body = body.replace(image, mdImg); // replace url in body for md link to downloaded file
-    }, function(err){
-        // if any of the file processing produced an error, err would equal that error
-        if( err ) {
-          // One of the iterations produced an error.
-          // All processing will now stop.
-          console.log('A file failed to process');
-        } else {
-          console.log('All files have been processed successfully');
-        }
+    // download main image
+    download(mainImage, projectFolder + '\/' + extractFileName(mainImage), function(){
+        console.log('downloaded main image ' + extractFileName(mainImage));
     });
-  }
 
-  md += "\n" + body;
-  // end of body
+    // start of body
+    var body = row.fullprojectdescription;
+    var images = body.match(/[^\s]*imgur[^\s]*/ig);
 
-  // write project md file
-  fs.writeFile('responses/' + projectSlug + '.md', md, function (err) {
-    if (err) throw err;
-    console.log('Saved ' + projectSlug + ' project.');
+    // download images
+    if (!images) images = [];
+    async.map(images,
+      function (image, callback) {
+        impurge.purge(image, function (error, urls) {
+          callback(error, [image, urls[0]]);
+        });
+      },
+      function (err, results) {
+        results.forEach(function (result) {
+          var originalImageUrl = result[0];
+          var fixedImageUrl = result[1];
+          var fileName = extractFileName(fixedImageUrl);
+          download(fixedImageUrl, projectFolder + '\/' + fileName, function(err, result) {
+            console.log('downloaded ' + fileName);
+          });
+          var mdImg = '![]({{ page.img-folder }}' + fileName + ')';
+          body = body.replace(originalImageUrl, mdImg); // replace url in body for md link to downloaded file
+        });
+
+        md += "\n" + body;
+        // end of body
+
+        // write project md file
+        fs.writeFile('responses/' + projectSlug + '.md', md, function (err) {
+          if (err) throw err;
+          console.log('Saved ' + projectSlug + ' project.');
+        });
+      }
+    );
   });
-}
-
-function parseImgLink(link) {
-  if (link.search('.png') > -1 ||
-      link.search('.jpg') > -1 ||
-      link.search('.jpeg') > -1 ||
-      link.search('.gif') > -1) {
-    return link;
-  } else if (link.search('imgur.com') >  -1) {
-    return link.replace('imgur.com', 'i.imgur.com') + '.jpg';
-  } else {
-    return link;
-  }
 }
 
 // http://stackoverflow.com/questions/12740659/downloading-images-with-node-js
